c64452845dd6deda4fe05d21715aa807cd7f8d4e
nugowe <nosa.ugowe@yahoo.com>
2022-03-11 15:48:52 -0600

    made changes to the build.sh script

--- build.sh
+++ build.sh
@@ -4,3 +4,22 @@ set -x
 
 
 Rscript /opt/epl/scripts/EPLTable.R
+
+INDEX_BUILD (){
+
+CURRENT_TIME=$(date "+%Y.%m.%d-%H.%M.%S")    
+echo """
+  epl-table_logs:
+    version: 1
+      logfile-name: s3://$S3_BUCKET/index/$EPL_FILE
+        timestamp: $CURRENT_TIME
+
+""" > index.yaml
+
+
+aws s3 cp index.yaml s3://$S3_BUCKET/index/index.yaml
+
+aws cp s3://$S3_BUCKET/index/index.yaml index.yaml
+
+}
+
