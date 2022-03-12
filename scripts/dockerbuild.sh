4198fbea3f98c8295d4d2691ad3da8a1e7177ac0
nugowe <nosa.ugowe@yahoo.com>
2022-03-12 08:06:32 -0600

    made changes to dockerbuild script

--- scripts/dockerbuild.sh
+++ scripts/dockerbuild.sh
@@ -2,3 +2,9 @@
 
 docker build -t nosaugowe/epl .
 
+CONTAINERID=$(cat /etc/hostname)
+
+docker run -d --name epltable nosaugowe/epltable
+
+docker cp ${CONTAINERID}:/opt/crypto/scripts/epltable.png /tmp
+
