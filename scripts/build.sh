334c3744f1856cee0f2d601458dbd39ce4729c6a
nugowe <nosa.ugowe@yahoo.com>
2022-03-13 11:52:20 -0500

    made changes to pipeline yaml

--- scripts/build.sh
+++ scripts/build.sh
@@ -10,46 +10,6 @@ Rscript /opt/epl/EPLTable.R
 aws s3api create-bucket --bucket $S3_BUCKET --region $AWS_DEFAULT_REGION --create-bucket-configuration LocationConstraint=$AWS_DEFAULT_REGION || true
 
 
-
-INDEX_BUILD (){
-echo "Initializing the INDEX_BUILD Function........."
-CURRENT_TIME=$(echo "$(date +'%Y-11-%d %H:%M:%S')")   
-echo """
-  epl-table_logs:
-    version: 1
-      logfile-name: s3://$S3_BUCKET/index/$EPL_FILE
-        timestamp: $CURRENT_TIME
-
-""" > index_build.yaml
-
-echo "Copying initial index file...................."
-aws s3 cp index_build.yaml s3://$S3_BUCKET/index/index.yaml
-
-
-
-}
-
-
-PIPELINE_INDEX_ADD () {
-echo "Initializing the S3_PUBLISH Function........."
-CURRENT_TIME=$(echo "$(date +'%Y-11-%d %H:%M:%S')")   
-aws s3 cp s3://$S3_BUCKET/index/index.yaml index_pipeline_add.yaml
-tail -n 5 index_pipeline_add.yaml | awk "/version/{print$2}" | awk -F":" '{print $2}' > index_observe.yaml
-summation=1
-old_index_value=$(cat index_observe.yaml)    
-sum=$(( $old_index_value + $summation ))
-echo """
-  epl-table_logs:
-    version: $sum
-      logfile-name: s3://$S3_BUCKET/index/$EPL_PNG_FILE 
-        timestamp: $CURRENT_TIME
-
-""" >> index_pipeline_add.yaml
-
-aws s3 cp index_pipeline_add.yaml s3://$S3_BUCKET/index/index.yaml
-
-}
-
 #checking if the an index file already exists
 aws s3api list-objects --bucket $S3_BUCKET --query "Contents[?contains(Key, 'index.yaml')]" > files.txt
 
@@ -57,15 +17,56 @@ INDEX_FILE=$(cat files.txt | awk "/Key/{print$1}" | awk -F":" '{print $2}' | awk
 
 
 if [[ $INDEX_FILE!="index.yaml" ]]; then
-  INDEX_BUILD && PIPELINE_INDEX_ADD
-  echo "Publishing the EPL Dashboard to s3........"
-  aws s3 sync ./$EPL_PNG_FILE s3://$S3_BUCKET/ 
+    echo "Initializing the INDEX_BUILD Function........."
+    CURRENT_TIME=$(echo "$(date +'%Y-11-%d %H:%M:%S')")   
+    echo """
+    epl-table_logs:
+        version: 1
+            logfile-name: s3://$S3_BUCKET/index/$EPL_FILE
+                timestamp: $CURRENT_TIME
+
+    """ > index_build.yaml
+
+    echo "Copying initial index file...................."
+    aws s3 cp index_build.yaml s3://$S3_BUCKET/index/index.yaml
+    
+
+    echo "Initializing the index sum Function........."
+    CURRENT_TIME=$(echo "$(date +'%Y-11-%d %H:%M:%S')")   
+    aws s3 cp s3://$S3_BUCKET/index/index.yaml index_pipeline_add.yaml
+    tail -n 5 index_pipeline_add.yaml | awk "/version/{print$2}" | awk -F":" '{print $2}' > index_observe.yaml
+    summation=1
+    old_index_value=$(cat index_observe.yaml)    
+    sum=$(( $old_index_value + $summation ))
+    echo """
+    epl-table_logs:
+        version: $sum
+            logfile-name: s3://$S3_BUCKET/index/$EPL_PNG_FILE 
+                timestamp: $CURRENT_TIME
+
+    """ >> index_pipeline_add.yaml
+
+    aws s3 cp index_pipeline_add.yaml s3://$S3_BUCKET/index/index.yaml
+ 
 fi
 
 if [[ $INDEX_FILE=="index.yaml" ]]; then
-  PIPELINE_INDEX_ADD
-  echo "Publishing the EPL Dashboard to s3........"
-  aws s3 sync ./$EPL_PNG_FILE s3://$S3_BUCKET/ 
+    echo "Initializing the index sum Function........."
+    CURRENT_TIME=$(echo "$(date +'%Y-11-%d %H:%M:%S')")   
+    aws s3 cp s3://$S3_BUCKET/index/index.yaml index_pipeline_add.yaml
+    tail -n 5 index_pipeline_add.yaml | awk "/version/{print$2}" | awk -F":" '{print $2}' > index_observe.yaml
+    summation=1
+    old_index_value=$(cat index_observe.yaml)    
+    sum=$(( $old_index_value + $summation ))
+    echo """
+    epl-table_logs:
+        version: $sum
+            logfile-name: s3://$S3_BUCKET/index/$EPL_PNG_FILE 
+                timestamp: $CURRENT_TIME
+
+    """ >> index_pipeline_add.yaml
+
+    aws s3 cp index_pipeline_add.yaml s3://$S3_BUCKET/index/index.yaml
 
 fi
 
