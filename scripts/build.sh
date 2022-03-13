78102bb87283dcfecc9627027bcf3ca372011b90
nugowe <nosa.ugowe@yahoo.com>
2022-03-13 07:14:36 -0500

    made changes to Dockerfile

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,7 +6,7 @@ echo "Running R script"
 
 Rscript /opt/epl/EPLTable.R 
 
-aws s3 /opt/epl/EPLTable.png s3://epltable/epl/EPLTable.png
+aws s3 cp /opt/epl/EPLTable.png s3://epltable/epl/EPLTable.png
 
 
 
