23a9d3d2a12490de53663edb35e5efd1b7a1df85
nugowe <nosa.ugowe@yahoo.com>
2023-07-02 15:27:25 -0500

    Made changes to the shell github actions

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,11 +3,11 @@
 
 
 
-if [[ ${{ github.event.client_payload.league }} == 'epl' ]]; then
+if [[ ${{ echo github.event.client_payload.league }} == 'epl' ]]; then
     echo "app_image=nosaugowe/epltable:latest" > $GITHUB_WORKSPACE/terraform/terraform.tfvars 
     echo "Revealing the updated config...."
     cat $GITHUB_WORKSPACE/terraform/terraform.tfvars 
-elif [[ ${{ github.event.client_payload.league }} == 'laliga' ]]; then
+elif [[ ${{ echo github.event.client_payload.league }} == 'laliga' ]]; then
     echo "app_image=nosaugowe/laliga:latest" > $GITHUB_WORKSPACE/terraform/terraform.tfvars 
     echo "Revealing the updated config...."
     cat $GITHUB_WORKSPACE/terraform/terraform.tfvars 
