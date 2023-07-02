2b0a670537418aeff9018778da72d47a8acfec6d
nugowe <nosa.ugowe@yahoo.com>
2023-07-02 16:09:28 -0500

    Made changes to the helm chart manifest

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/bash
 
 #if [[ -f $GITHUB_WORKSPACE/terraform/terraform.tfvars ]]; then
     #continue
@@ -13,10 +13,12 @@ if [[ ${{ github.event.client_payload.league }} == 'epl' ]] ; then
     #echo "app_image=nosaugowe/epltable:latest" > $GITHUB_WORKSPACE/terraform/terraform.tfvars 
     #echo "Revealing the updated config...."
     #cat $GITHUB_WORKSPACE/terraform/terraform.tfvars 
+    echo "epl payload detected!"
 elif [[ ${{ github.event.client_payload.league }} == 'laliga' ]] ; then
     #echo "app_image=nosaugowe/laliga:latest" > $GITHUB_WORKSPACE/terraform/terraform.tfvars 
     #echo "Revealing the updated config...."
     #cat $GITHUB_WORKSPACE/terraform/terraform.tfvars 
+    echo "laliga payload detected!"
 else
     echo "No payload variable found!"
 fi
