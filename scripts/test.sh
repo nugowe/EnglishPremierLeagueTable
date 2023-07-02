1c2ac21438eb09d689cd0c4a5931d7ca337ea090
nugowe <nosa.ugowe@yahoo.com>
2023-07-02 15:38:14 -0500

    Made changes to the shell github actions

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
+if [[ -f $GITHUB_WORKSPACE/terraform/terraform.tfvars ]]; then
+    continue
+else
+    echo "Both variables.tf && terraform.tfvars files are missing!"
+fi
 
 
 
-if [[ ${{ echo github.event.client_payload.league }} == 'epl' ]]; then
-    echo "app_image=nosaugowe/epltable:latest" > $GITHUB_WORKSPACE/terraform/terraform.tfvars 
-    echo "Revealing the updated config...."
-    cat $GITHUB_WORKSPACE/terraform/terraform.tfvars 
-elif [[ ${{ echo github.event.client_payload.league }} == 'laliga' ]]; then
-    echo "app_image=nosaugowe/laliga:latest" > $GITHUB_WORKSPACE/terraform/terraform.tfvars 
-    echo "Revealing the updated config...."
-    cat $GITHUB_WORKSPACE/terraform/terraform.tfvars 
+
+if [[ ${{ github.event.client_payload.league }} == 'epl' ]]; then
+    #echo "app_image=nosaugowe/epltable:latest" > $GITHUB_WORKSPACE/terraform/terraform.tfvars 
+    #echo "Revealing the updated config...."
+    #cat $GITHUB_WORKSPACE/terraform/terraform.tfvars 
+elif [[ ${{ github.event.client_payload.league }} == 'laliga' ]]; then
+    #echo "app_image=nosaugowe/laliga:latest" > $GITHUB_WORKSPACE/terraform/terraform.tfvars 
+    #echo "Revealing the updated config...."
+    #cat $GITHUB_WORKSPACE/terraform/terraform.tfvars 
+else
+    echo "No payload variable found!"
 fi
