6d8039fa3c17269044a4067d8848720565e2df93
nugowe <nosa.ugowe@yahoo.com>
2023-07-02 16:43:26 -0500

    Made changes to the helm chart

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/bash
 
+
+
 #if [[ -f $GITHUB_WORKSPACE/terraform/terraform.tfvars ]]; then
     #continue
 #else
     #echo "Both variables.tf && terraform.tfvars files are missing!"
-#fi
-
-
 
 
 if [[ ${{ github.event.client_payload.league }} == 'epl' ]] ; then
